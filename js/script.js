fd5b077d28a661c1c30170c86d14b403b38e72c3
RonivalSantos <ronivalsantos16@gmail.com>
2025-03-10 18:03:41 -0300

    alterações adicionais

--- js/script.js
+++ js/script.js
@@ -5,19 +5,35 @@ mobileBtn.onclick = () => {
   mobileBtn.classList.toggle("trocar");
 };
 
-const leftBtn = document.querySelector(".botao-esquerdo");
-const rightBtn = document.querySelector(".botao-direito");
-const local = document.querySelector(".local");
-const telefone = document.querySelector(".telefone");
-const email = document.querySelector(".email");
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
 
-rightBtn.onclick = () => {
-  telefone.classList.add("primeiro");
-  local.classList.remove("primeiro");
-};
-// rightBtn.onclick = (evento) => {
-//   if ((evento = 1)) {
-//     telefone.classList.add("primeiro");
-//     local.classList.remove("primeiro");
-//   }
+function scrollToSection(event) {
+  event.preventDefault();
+  const href = event.currentTarget.getAttribute("href");
+  const section = document.querySelector(href);
+  
+  section.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+  });
+}
+linksInternos.forEach((link) => {
+  link.addEventListener("click", scrollToSection);
+});
+
+// const leftBtn = document.querySelector(".botao-esquerdo");
+// const rightBtn = document.querySelector(".botao-direito");
+// const local = document.querySelector(".local");
+// const telefone = document.querySelector(".telefone");
+// const email = document.querySelector(".email");
+
+// rightBtn.onclick = () => {
+//   telefone.classList.add("primeiro");
+//   local.classList.remove("primeiro");
 // };
+// // rightBtn.onclick = (evento) => {
+// //   if ((evento = 1)) {
+// //     telefone.classList.add("primeiro");
+// //     local.classList.remove("primeiro");
+// //   }
+// // };
